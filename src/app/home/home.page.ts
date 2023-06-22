367ba1082890b03bf55886f0a512355126f78ea2
Rayane Soares <117865429+RaySoares@users.noreply.github.com>
2023-06-22 23:26:46 +0000

    calculator

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,8 +6,37 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule]
 })
+
 export class HomePage {
-  constructor() {}
+  
+  // displayValue
+  displayValue = '';
+  // botões da calculadora
+  buttons = ['c', '( )', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0'];
+
+  buttonClick(button: string) {
+    // valor do botão e valor exibido no displayValue
+    document.getElementById('displayValue')!.innerHTML += button;
+  }
+
+  cleardisplayValue() {
+    // Limpa o valor exibido no displayValue
+    document.getElementById('displayValue')!.innerHTML = "";
+  }
+  back() {
+    const element = document.getElementById('displayValue');
+    if (element) {
+      const resultado = element.textContent!;
+      element.textContent = resultado.slice(0, -1);
+    }
+  }
+
+  calculateResult() {
+    let resultado = document.getElementById('displayValue')!.innerHTML;
+    if (resultado) {
+      document.getElementById('displayValue')!.innerHTML = eval(resultado);
+    }
+  }
 }
